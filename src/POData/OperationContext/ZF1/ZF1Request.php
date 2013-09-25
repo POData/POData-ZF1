2f76e9b6324ec8d733f22b53d89c4b24f880e2b7
drdamour <drdamour@gmail.com>
2013-09-25 16:49:31 -0600

    IMplementes Request pieces

--- src/POData/OperationContext/ZF1/ZF1Request.php
+++ src/POData/OperationContext/ZF1/ZF1Request.php
@@ -2,8 +2,94 @@
 
 namespace POData\OperationContext\ZF1;
 
-class ZF1Request
+use POData\Common\ODataConstants;
+use POData\OperationContext\HTTPRequestMethod;
+use POData\OperationContext\IHTTPRequest;
+
+class ZF1Request implements IHTTPRequest
 {
 
+	/**
+	 * @var \Zend_Controller_Request_Http
+	 */
+	protected $request;
 
+	/**
+	 * @var string
+	 */
+	protected $overriddenAccept;
+
+	public function __construct(\Zend_Controller_Request_Http $zendRequest  )
+	{
+		$this->request = $zendRequest;
+	}
+
+	/**
+	 * get the raw incoming url
+	 *
+	 * @return string RequestURI called by User with the value of QueryString
+	 */
+	public function getRawUrl()
+	{
+		return $this->request->getRequestUri();
+	}
+
+	/**
+	 * get the specific request headers
+	 *
+	 * @param string $key The header name
+	 *
+	 * @return string|null value of the header, NULL if header is absent.
+	 */
+	public function getRequestHeader($key)
+	{
+		if($key === ODataConstants::HTTPREQUEST_HEADER_ACCEPT && !is_null($this->overriddenAccept)){
+			//Some call was made to setRequestAccept
+			return $this->overriddenAccept;
+		}
+
+		$result = $this->request->getHeader($key);
+		//Zend returns false for a missing header...POData needs a null
+		if($result === false) return null;
+
+		return $result;
+
+	}
+
+	/**
+	 * Split the QueryString and assigns them as array element in KEY=VALUE
+	 *
+	 * @return string[]
+	 */
+	public function getQueryParameters()
+	{
+		//TODO: the contract is more specific than this, it requires the name and values to be decoded
+		//not sure how to test that...
+		//TODO: another issue.  This may not be the right thing to return...since POData only really understands GET requests today
+		return $this->request->getParams();
+	}
+
+	/**
+	 * Get the HTTP method/verb of the HTTP Request
+	 *
+	 * @return HTTPRequestMethod
+	 */
+	public function getMethod()
+	{
+		return new HTTPRequestMethod($this->request->getMethod());
+	}
+
+	/**
+	 * To change the request accept type header in the request.
+	 * Note: This method will be used only when client specified $format query option.
+	 * Any subsequent call to getRequestHeader("HTTP_ACCEPT") must return the value set with this call
+	 *
+	 * @param string $mime The mime value.
+	 *
+	 * @return void
+	 */
+	public function setRequestAccept($mime)
+	{
+		$this->overriddenAccept = $mime;
+	}
 }
--- tests/UnitTests/POData/OperationContext/ZF1/ZF1RequestTest.php
+++ tests/UnitTests/POData/OperationContext/ZF1/ZF1RequestTest.php
@@ -3,14 +3,160 @@
 namespace UnitTests\POData\OperationContext\ZF1;
 
 
+use POData\Common\ODataConstants;
+use POData\OperationContext\HTTPRequestMethod;
 use POData\OperationContext\ZF1\ZF1Request;
+use Phockito;
+
 
 class ZF1RequestTest extends \PHPUnit_Framework_TestCase
 {
 
-	public function testConstructor(){
-		$req = new ZF1Request();
+	/** @var  \Zend_Controller_Request_Http */
+	protected $mockRequest;
 
+	public function setUp()
+	{
+		Phockito::include_hamcrest();
+		$this->mockRequest = Phockito::mock('Zend_Controller_Request_Http');
+	}
+
+	public function testConstructor(){
+
+		$req = new ZF1Request($this->mockRequest );
+
+	}
+
+	public function testGetMethod()
+	{
+		Phockito::when($this->mockRequest->getMethod())
+			->return("GET");
+
+		$req = new ZF1Request($this->mockRequest );
+
+		$actual = $req->getMethod();
+
+		$expected = HTTPRequestMethod::GET();
+
+		$this->assertEquals($expected, $actual);
+	}
+
+
+	public function testGetMethodUnknown()
+	{
+		Phockito::when($this->mockRequest->getMethod())
+			->return("UNKOWN");
+
+		$req = new ZF1Request($this->mockRequest );
+
+		try{
+			$req->getMethod();
+			$this->fail("Expected exception not thrown");
+		}
+		catch(\Exception $ex) {
+			$this->assertEquals("Value 'UNKOWN' is not part of the enum POData\OperationContext\HTTPRequestMethod", $ex->getMessage());
+		}
+
+	}
+
+	public function testGetRawUrl()
+	{
+		$expected = "http://someHost/SomeUrl/SomePath/" . uniqid();
+
+		Phockito::when($this->mockRequest->getRequestUri())
+			->return($expected);
+		$req = new ZF1Request($this->mockRequest );
+
+		$actual = $req->getRawUrl();
+
+
+		$this->assertSame($expected, $actual);
+	}
+
+
+	public function testGetHeader()
+	{
+
+		$header = "SomeWhackyHeader" . uniqid();
+
+		$expected = "SomeHeaderValue" . uniqid();
+
+		Phockito::when($this->mockRequest->getHeader($header))
+			->return($expected);
+
+		$req = new ZF1Request($this->mockRequest );
+
+
+		$actual = $req->getRequestHeader($header);
+
+
+
+		$this->assertSame($expected, $actual);
+	}
+
+
+	public function testGetHeaderNonExistent()
+	{
+		$nonExistentHeader = "SomeWhackyHeader" . uniqid();
+
+		//Zend returns false when a header isn't present
+		Phockito::when($this->mockRequest->getHeader($nonExistentHeader))
+			->return(false);
+
+		$req = new ZF1Request($this->mockRequest );
+
+		$actual = $req->getRequestHeader($nonExistentHeader);
+
+		$this->assertNull($actual);
+	}
+
+	public function testGetQueryParameters()
+	{
+
+		$expected = array(
+			'$format' => 'json',
+			'$filter' => "Property eq '1'",
+		);
+
+		//This may be the wrong thing to mock..see notes in implementation
+		Phockito::when($this->mockRequest->getParams())
+			->return($expected);
+
+		$req = new ZF1Request($this->mockRequest );
+
+		$actual = $req->getQueryParameters();
+
+
+		$this->assertSame($expected, $actual);
+	}
+
+	public function testSetRequestAccept()
+	{
+		$header = ODataConstants::HTTPREQUEST_HEADER_ACCEPT;
+
+		$expected = "SomeHeaderValue" . uniqid();
+
+		Phockito::when($this->mockRequest->getHeader($header))
+			->return($expected);
+
+		$req = new ZF1Request($this->mockRequest );
+
+		//Get the header before to ensure it has a value
+		$actual = $req->getRequestHeader($header);
+
+		$this->assertSame($expected, $actual);
+
+
+		//Now set the new value (because $format is specified)
+		$fakeMime = "some new format" . uniqid();
+		$req->setRequestAccept($fakeMime);
+
+
+		//and ensure the retrieval is the save value
+		$actual = $req->getRequestHeader($header);
+
+
+		$this->assertSame($fakeMime, $actual);
 	}
 
 }
