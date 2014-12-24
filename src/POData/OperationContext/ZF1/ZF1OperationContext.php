a9f08ad0a108123392a15b90ea861df732ec6277
drdamour <drdamour@gmail.com>
2014-12-24 16:35:55 -0700

    fixed indentation

--- src/POData/OperationContext/ZF1/ZF1OperationContext.php
+++ src/POData/OperationContext/ZF1/ZF1OperationContext.php
@@ -9,40 +9,40 @@ use POData\OperationContext\Web\OutgoingResponse;
 class ZF1OperationContext implements IOperationContext
 {
 
-	/**
-	 * @var ZF1Request;
-	 */
-	protected $request;
+    /**
+     * @var ZF1Request;
+     */
+    protected $request;
 
-	protected $response;
+    protected $response;
 
-	/**
-	 * @param \Zend_Controller_Request_Http $request
-	 */
-	public function __construct(\Zend_Controller_Request_Http $request){
-		$this->request = new ZF1Request($request);
-		$this->response = new OutgoingResponse();
-	}
+    /**
+     * @param \Zend_Controller_Request_Http $request
+     */
+    public function __construct(\Zend_Controller_Request_Http $request){
+        $this->request = new ZF1Request($request);
+        $this->response = new OutgoingResponse();
+    }
 
-	/**
-	 * Gets the Web request context for the request being sent.
-	 *
-	 * @return OutgoingResponse reference of OutgoingResponse object
-	 */
-	public function outgoingResponse()
-	{
-		return $this->response;
-	}
+    /**
+     * Gets the Web request context for the request being sent.
+     *
+     * @return OutgoingResponse reference of OutgoingResponse object
+     */
+    public function outgoingResponse()
+    {
+        return $this->response;
+    }
 
 
-	/**
-	 * Gets the Web request context for the request being received.
-	 *
-	 * @return IHTTPRequest reference of IncomingRequest object
-	 */
-	public function incomingRequest()
-	{
-		return $this->request;
-	}
+    /**
+     * Gets the Web request context for the request being received.
+     *
+     * @return IHTTPRequest reference of IncomingRequest object
+     */
+    public function incomingRequest()
+    {
+        return $this->request;
+    }
 
 }
